ad0e1a0114e4e6f86457fb65c31ca030a306128f
Stephen Zeiler <stephennnlz@gmail.com>
2026-01-27 18:14:17 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -308,7 +308,8 @@ void setup() {
   // If Arduino drives a strong HIGH/LOW, plain INPUT is fine.
   // If you need a default LOW, you can use INPUT_PULLDOWN on many Teensy boards.
   //pinMode(PIN_INJECT_CMD, INPUT);
-    pinMode(PIN_INJECT_CMD, INPUT);
+    pinMode(PIN_INJECT_CMD, INPUT_PULLUP);
+
   // Outputs to Arduino
   pinMode(PIN_HOME_READY, OUTPUT);
   pinMode(PIN_OVERRUN_ALARM, OUTPUT);
